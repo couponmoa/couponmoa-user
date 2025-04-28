296c62322a0e6cb48abc9f28c8d30df06ad680a5
나연 <kny5579@naver.com>
2025-04-28 15:04:25 +0900

    feat(#1): gRPC 서비스 로직 추가

--- src/main/java/com/couponmoa/backend/couponmoauser/domain/user/grpc/UserGrpcService.java
+++ src/main/java/com/couponmoa/backend/couponmoauser/domain/user/grpc/UserGrpcService.java
@@ -1,18 +1,20 @@
 package com.couponmoa.backend.couponmoauser.domain.user.grpc;
 import com.couponmoa.backend.couponmoauser.domain.user.entity.User;
+import com.couponmoa.backend.couponmoauser.domain.user.repository.UserRepository;
 import com.couponmoa.backend.couponmoauser.domain.user.service.v1.UserServiceV1;
-import com.couponmoa.grpc.user.UserIdRequest;
-import com.couponmoa.grpc.user.UserResponse;
-import com.couponmoa.grpc.user.UserServiceGrpc;
+import com.couponmoa.grpc.user.*;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.util.List;
 
 @GrpcService
 @RequiredArgsConstructor
 public class UserGrpcService extends UserServiceGrpc.UserServiceImplBase {
 
     private final UserServiceV1 userService;
+    private final UserRepository userRepository;
 
     @Override
     public void findById(UserIdRequest request, StreamObserver<UserResponse> responseObserver) {
@@ -26,6 +28,18 @@ public class UserGrpcService extends UserServiceGrpc.UserServiceImplBase {
         responseBuilder.setNickname(user.getNickname());
         responseBuilder.setUserRole(user.getUserRole().toString());
         responseBuilder.setDeletedAt(deletedAt);
+
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getUserEmailList(UserIdsRequest request, StreamObserver<UserEmailsResponse> responseObserver) {
+        List<Long> userIdList = request.getUserIdsList();
+
+        List<String> emailList = userRepository.findAllById(userIdList).stream().map(User::getEmail).toList();
+        UserEmailsResponse.Builder responseBuilder = UserEmailsResponse.newBuilder();
+        responseBuilder.addAllEmails(emailList);
 
         responseObserver.onNext(responseBuilder.build());
         responseObserver.onCompleted();
