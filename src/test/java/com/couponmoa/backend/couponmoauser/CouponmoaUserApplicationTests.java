c70c24a268674c0b37289017e21f5c71e241b519
나연 <kny5579@naver.com>
2025-04-29 15:28:20 +0900

    fix(#5): 테스트 코드 빌드를 위한 test.yml 파일 작성

--- src/test/java/com/couponmoa/backend/couponmoauser/CouponmoaUserApplicationTests.java
+++ src/test/java/com/couponmoa/backend/couponmoauser/CouponmoaUserApplicationTests.java
@@ -2,12 +2,13 @@ package com.couponmoa.backend.couponmoauser;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CouponmoaUserApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
